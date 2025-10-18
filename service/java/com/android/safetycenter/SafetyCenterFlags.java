ab09199be10813000e362cfbab84098117c62f28
Dmitrii <bankersenator@gmail.com>
2025-10-19 05:27:56 +0800

    safetycenter: Force enable safety center
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- service/java/com/android/safetycenter/SafetyCenterFlags.java
+++ service/java/com/android/safetycenter/SafetyCenterFlags.java
@@ -242,15 +242,12 @@ public final class SafetyCenterFlags {
 
     /** Returns whether Safety Center is enabled. */
     public static boolean getSafetyCenterEnabled() {
-        if (Flags.safetyCenterEnabledNoDeviceConfig() && SdkLevel.isAtLeastU()) {
             return true;
-        }
-        return getBoolean(PROPERTY_SAFETY_CENTER_ENABLED, SdkLevel.isAtLeastU());
     }
 
     /** Returns whether Safety Center notifications are enabled. */
     public static boolean getNotificationsEnabled() {
-        return getBoolean(PROPERTY_NOTIFICATIONS_ENABLED, SdkLevel.isAtLeastU());
+        return true;
     }
 
     /**
